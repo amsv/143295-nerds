29d4f48a7704be7ae97dbc8ffe4b861b9b9252ac
amsv <ammosovaleksey@ya.ru>
2016-02-21 19:39:02 +0300

    поправил кнопки, испрвил ковычки

--- js/script.js
+++ js/script.js
@@ -39,21 +39,21 @@ window.addEventListener("keydown", function (event) {
 });
 
 ymaps.ready(function () {
-    var myMap = new ymaps.Map('yandex-map', {
+    var myMap = new ymaps.Map("yandex-map", {
             center: [59.938667, 30.323073],
             zoom: 17
         }, {
-            searchControlProvider: 'yandex#search'
+            searchControlProvider: "yandex#search"
         }),
         myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
-            hintContent: 'Собственный значок метки',
-            balloonContent: 'Это красивая метка'
+            hintContent: "Собственный значок метки",
+            balloonContent: "Это красивая метка"
         }, {
             // Опции.
             // Необходимо указать данный тип макета.
-            iconLayout: 'default#image',
+            iconLayout: "default#image",
             // Своё изображение иконки метки.
-            iconImageHref: 'img/map-point.png',
+            iconImageHref: "img/map-point.png",
             // Размеры метки.
             iconImageSize: [231, 190],
             // Смещение левого верхнего угла иконки относительно
